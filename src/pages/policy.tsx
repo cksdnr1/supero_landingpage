00cbe3b42af35e073088498282ba085236c2cb7e
Chanwook Lee <cksdnr1@gmail.com>
2023-05-07 14:54:25 +0900

    edit

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -1,10 +1,11 @@
-import { Footer } from '../templates/Footer';
-import { Hero } from '../templates/Hero';
+/* eslint-disable */
 import { Section } from '../layout/Section';
+import { Footer } from '../templates/Footer';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import Link from 'next/link';
 import { Logo } from '../templates/Logo';
 import { Background } from '../background/Background';
+
+import Link from 'next/link';
 
 const Policy = () => (
     <div className="antialiased text-white bg-[#101012]">
@@ -18,7 +19,7 @@ const Policy = () => (
                     </li>
                 </NavbarTwoColumns>
             </Section>
-            <Section>
+            <div>
                 Privacy Policy Supero.corp built the Supero app as a Free app.
                 This SERVICE is provided by Supero.corp at no cost and is
                 intended for use as is. This page is used to inform visitors
@@ -32,9 +33,8 @@ const Policy = () => (
                 used in this Privacy Policy have the same meanings as in our
                 Terms and Conditions, which are accessible at Supero unless
                 otherwise defined in this Privacy Policy.
-            </Section>
-
-            <Section>
+            </div>
+            <div>
                 Information Collection and Use For a better experience, while
                 using our Service, we may require you to provide us with certain
                 personally identifiable information, including but not limited
@@ -44,9 +44,8 @@ const Policy = () => (
                 services that may collect information used to identify you. Link
                 to the privacy policy of third-party service providers used by
                 the app
-            </Section>
-
-            <Section>
+            </div>
+            <div>
                 Google Play Services. Log Data We want to inform you that
                 whenever you use our Service, in a case of an error in the app
                 we collect data and information (through third-party products)
@@ -55,9 +54,8 @@ const Policy = () => (
                 address, device name, operating system version, the
                 configuration of the app when utilizing our Service, the time
                 and date of your use of the Service, and other statistics.
-            </Section>
-
-            <Section>
+            </div>
+            <div>
                 Cookies Cookies are files with a small amount of data that are
                 commonly used as anonymous unique identifiers. These are sent to
                 your browser from the websites that you visit and are stored on
@@ -68,14 +66,12 @@ const Policy = () => (
                 refuse these cookies and know when a cookie is being sent to
                 your device. If you choose to refuse our cookies, you may not be
                 able to use some portions of this Service.
-            </Section>
-
-            <Section>
+            </div>
+            <div>
                 Service Providers We may employ third-party companies and
                 individuals due to the following reasons:
-            </Section>
-
-            <Section>
+            </div>
+            <div>
                 To facilitate our Service; To provide the Service on our behalf;
                 To perform Service-related services; or To assist us in
                 analyzing how our Service is used. We want to inform users of
@@ -83,17 +79,17 @@ const Policy = () => (
                 Personal Information. The reason is to perform the tasks
                 assigned to them on our behalf. However, they are obligated not
                 to disclose or use the information for any other purpose.
-            </Section>
-            <Section>
+            </div>
+            <div>
                 Security We value your trust in providing us your Personal
                 Information, thus we are striving to use commercially acceptable
                 means of protecting it. But remember that no method of
                 transmission over the internet, or method of electronic storage
                 is 100% secure and reliable, and we cannot guarantee its
                 absolute security.
-            </Section>
+            </div>
 
-            <Section>
+            <div>
                 Links to Other Sites This Service may contain links to other
                 sites. If you click on a third-party link, you will be directed
                 to that site. Note that these external sites are not operated by
@@ -101,9 +97,9 @@ const Policy = () => (
                 Policy of these websites. We have no control over and assume no
                 responsibility for the content, privacy policies, or practices
                 of any third-party sites or services.
-            </Section>
+            </div>
 
-            <Section>
+            <div>
                 Children’s Privacy These Services do not address anyone under
                 the age of 13. We do not knowingly collect personally
                 identifiable information from children under 13 years of age. In
@@ -112,22 +108,22 @@ const Policy = () => (
                 servers. If you are a parent or guardian and you are aware that
                 your child has provided us with personal information, please
                 contact us so that we will be able to do the necessary actions.
-            </Section>
+            </div>
 
-            <Section>
+            <div>
                 Changes to This Privacy Policy We may update our Privacy Policy
                 from time to time. Thus, you are advised to review this page
                 periodically for any changes. We will notify you of any changes
                 by posting the new Privacy Policy on this page. This policy is
                 effective as of 2022-07-12
-            </Section>
-            <Section>
+            </div>
+            <div>
                 Contact Us If you have any questions or suggestions about our
                 Privacy Policy, do not hesitate to contact us at
                 supero.corp@gmail.com. This privacy policy page was created at
                 privacypolicytemplate.net and modified/generated by App Privacy
                 Policy Generator
-            </Section>
+            </div>
             <Footer />
         </Background>
     </div>
